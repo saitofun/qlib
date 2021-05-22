74166945c1dea1a660887c6dca8fb35452d48c75
SincosΣΩΔ <birdyfj@gmail.com>
2021-05-22 19:02:50 +0800

    ... tmp commit

--- database/qbuilder/qbuilder.go
+++ database/qbuilder/qbuilder.go
@@ -4,3 +4,13 @@ type Query struct {
 	Expr string
 	Args []interface{}
 }
+
+func Join(queries ...*Query) *Query {
+	ret := &Query{}
+	for _, q := range queries {
+		ret.Expr += q.Expr
+		ret.Expr += " "
+		ret.Args = append(ret.Args, q.Args...)
+	}
+	return ret
+}
