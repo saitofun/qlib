0a8b14c63d7a1227d15fa9924d5a2b17c9da4c44
SincosΣΩΔ <birdyfj@gmail.com>
2021-05-24 17:45:22 +0800

    feat: qsync.Locker

---
+++ os/qsync/qtype_locker.go
@@ -0,0 +1,16 @@
+package qsync
+
+import "git.querycap.com/ss/lib/container/qtype"
+
+const (
+	free = false
+	busy = true
+)
+
+type Locker qtype.Bool
+
+func NewLocker() *Locker { return (*Locker)(qtype.NewBool()) }
+
+func (l *Locker) Lock() bool { return (*qtype.Bool)(l).CAS(free, busy) }
+
+func (l *Locker) Unlock() { (*qtype.Bool)(l).Set(free) }
