362f6d674c4538428fd38e56236e5692fb05c17e
SincosΣΩΔ <fangjian@rockontrol.com>
2021-08-02 18:57:53 +0800

    feat: qcmd.Pid

--- database/qbuilder/qbuilder_def_table.go
+++ database/qbuilder/qbuilder_def_table.go
@@ -2,7 +2,6 @@ package qbuilder
 
 import (
 	"errors"
-	"go/ast"
 	"reflect"
 	"sync"
 )
@@ -178,55 +177,55 @@ func (s *Schema) NewSelectDestSliceWithCap(cap int) reflect.Value {
 }
 
 func (s *Schema) ParseField(fs reflect.StructField, fv reflect.Value, fields []*Field) {
-	if ast.IsExported(fs.Name) {
-		return
-	}
-	tag, ok := fs.Tag.Lookup("db")
-	if fs.Anonymous && !ok {
-		for i := 0; i < fv.NumField(); i++ {
-			s.ParseField(fv.Type().Field(i), fv.Field(i), fields)
-		}
-		return
-	}
+	// if ast.IsExported(fs.Name) {
+	// 	return
+	// }
+	// tag, ok := fs.Tag.Lookup("db")
+	// if fs.Anonymous && !ok {
+	// 	for i := 0; i < fv.NumField(); i++ {
+	// 		s.ParseField(fv.Type().Field(i), fv.Field(i), fields)
+	// 	}
+	// 	return
+	// }
 
-	f := &Field{
-		Name:         fs.Name,
-		Column:       "",
-		Schema:       s,
-		Tags:         ParseTags(fs.Tag, ","),
-		DataType:     "",
-		SQLType:      "",
-		Struct:       fs,
-		Type:         fs.Type,
-		IndirectType: fs.Type,
-	}
-	for f.IndirectType.Kind() == reflect.Ptr {
-		f.IndirectType = f.IndirectType.Elem()
-	}
+	// f := &Field{
+	// 	Name:         fs.Name,
+	// 	Column:       "",
+	// 	Schema:       s,
+	// 	Tags:         ParseTags(fs.Tag, ","),
+	// 	DataType:     "",
+	// 	SQLType:      "",
+	// 	Struct:       fs,
+	// 	Type:         fs.Type,
+	// 	IndirectType: fs.Type,
+	// }
+	// for f.IndirectType.Kind() == reflect.Ptr {
+	// 	f.IndirectType = f.IndirectType.Elem()
+	// }
 
-	fv := reflect.New(f.IndirectType)
+	// fv := reflect.New(f.IndirectType)
 
-	if c, ok := fv.Interface().(WithColumnName); ok {
-		f.Column = c.ColumnName()
-	} else {
-		if f.Column = f.Tags.ColumnName(); f.Column == "" {
-			f.Column = NamingStrategy.ColumnName(s.TableName, f.Name)
-		}
-	}
+	// if c, ok := fv.Interface().(WithColumnName); ok {
+	// 	f.Column = c.ColumnName()
+	// } else {
+	// 	if f.Column = f.Tags.ColumnName(); f.Column == "" {
+	// 		f.Column = NamingStrategy.ColumnName(s.TableName, f.Name)
+	// 	}
+	// }
 
-	if t, ok := fv.Interface().(WithSQLType); ok {
-		f.SQLType = t.SQLType()
-	} else {
-		switch f.IndirectType.Kind() {
-		case reflect.Bool:
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		case reflect.Float32, reflect.Float64:
-		case reflect.String:
-		case reflect.Struct:
-		case reflect.Slice, reflect.Array:
-		}
-	}
+	// if t, ok := fv.Interface().(WithSQLType); ok {
+	// 	f.SQLType = t.SQLType()
+	// } else {
+	// 	switch f.IndirectType.Kind() {
+	// 	case reflect.Bool:
+	// 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+	// 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+	// 	case reflect.Float32, reflect.Float64:
+	// 	case reflect.String:
+	// 	case reflect.Struct:
+	// 	case reflect.Slice, reflect.Array:
+	// 	}
+	// }
 	return
 }
 
